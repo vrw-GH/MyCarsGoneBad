905cd2bffdd5ff08e4145cb606e03819d74bc4bc
Victor Wright <victor.wright@outlook.de>
2024-06-19 17:52:11 +0200

    various
    css trials

--- assets/controllers/closeable_controller.js
+++ assets/controllers/closeable_controller.js
@@ -2,18 +2,18 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
    async toggle() {
-      if (this.element.style.width === '2rem') {
+      if (this.element.style.width !== 'auto') {
          this.element.style.width = 'auto';
          this.element.children[0].children[0].style.transform = 'rotate(0deg)';
       } else {
-         this.element.style.width = '2rem';
+         this.element.style.width = '1.8rem';
          this.element.children[0].children[0].style.transform = 'rotate(180deg)';
       }
       await this.#waitForAnimation();
    };
 
    async close() {
-      this.element.style.width = '2rem';
+      this.element.style.width = '1.8rem';
       await this.#waitForAnimation();
       this.element.remove();
    }
